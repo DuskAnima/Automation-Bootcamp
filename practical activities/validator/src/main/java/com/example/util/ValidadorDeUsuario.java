385238f09b3362c40e5c9dbcdf533b628f37f7aa
Anima <cjaccardh@gmail.com>
2025-07-19 00:03:02 -0400

    mockito practice

---
+++ practical activities/validator/src/main/java/com/example/util/ValidadorDeUsuario.java
@@ -0,0 +1,29 @@
+package com.example.util;
+
+import java.util.regex.Pattern;
+
+import com.example.modelo.Usuario;
+
+public class ValidadorDeUsuario {
+    private static final Pattern EMAIL_PATTERN = Pattern.compile("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\[A-Za-z]{2,}$");
+
+    public boolean esNombreValido(String nombre) {
+        return nombre!= null && !nombre.trim().isEmpty() && nombre.length() >= 3;
+    }
+
+    public boolean esEmailValido(String email) {
+        return email != null && EMAIL_PATTERN.matcher(email).matches();
+    }
+
+    public boolean esMayorDeEdad(int edad) {
+        return edad <= 18;
+    }
+
+    public boolean valudadorUsuario(Usuario usuario) {
+        return  esNombreValido(usuario.getNombre()) &&
+                esEmailValido(usuario.getEmail()) &&
+                esMayorDeEdad(usuario.getEdad());
+    }
+
+
+}
