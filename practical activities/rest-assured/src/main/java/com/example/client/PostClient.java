481dfa9de34d6aa9233b9fd8435f02be9377afe5
Anima <cjaccardh@gmail.com>
2025-09-10 22:25:28 -0300

    modelo json terminado

--- practical activities/rest-assured/src/main/java/com/example/client/PostClient.java
+++ practical activities/rest-assured/src/main/java/com/example/client/PostClient.java
@@ -3,15 +3,16 @@ package com.example.client;
 public class PostClient {
   
   public PostClient() {
+    // Lista para posts
+
     
+    // Obtener el detalle de un post por id
+    
+    // Crear un post
+    
+    // Actualizar un post
+    
+    // Eliminar un post
+  
   }
-  // Lista para posts
-
-  // Obtener el detalle de un post por id
-
-  // Crear un post
-
-  // Actualizar un post
-
-  // Eliminar un post
 }
